d33d7dd6b6d470d24ab7f84203f7a4e6681b2874
Shashaev <shashaevkirill@gmail.com>
2026-02-18 22:27:40 +0300

    add style, add config .flake8

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
+import math
+
+import PIL.Image
+import PIL.ImageDraw
 import pygame
-from math import acos, pi, tan
-from PIL import Image, ImageDraw
 
 
 class Bol:
-    def __init__(self, x0: float, y0: float, radius: float, direction: list[float, float]):
+    def __init__(
+        self,
+        x0: float,
+        y0: float,
+        radius: float,
+        direction: tuple[float, float],
+    ) -> None:
         self.x0, self.y0 = x0, y0
         self.r = radius
         self.x, self.y = direction
@@ -17,7 +25,7 @@ class Bol:
             self.x /= self.speed
             self.y /= self.speed
 
-    def run(self):
+    def run(self) -> None:
         self.x0 += self.x * self.speed
         self.y0 += self.y * self.speed
 
@@ -36,14 +44,14 @@ class Bol:
             self.y0 += abs(self.y0 - self.r)
             y1 += 1
         else:
-            return True
+            return
 
         cs = (-self.x * x1 + -self.y * y1) / (self.x ** 2 + self.y ** 2) ** 0.5
-        ar = pi / 2 - acos(cs)
+        ar = math.pi / 2 - math.acos(cs)
 
         if x1 == 0:
             if self.x != 0:
-                self.x = (1 / tan(ar)) * self.x / abs(self.x)
+                self.x = (1 / math.tan(ar)) * self.x / abs(self.x)
             else:
                 self.x = 0
         else:
@@ -51,7 +59,7 @@ class Bol:
 
         if y1 == 0:
             if self.y != 0:
-                self.y = (1 / tan(ar)) * self.y / abs(self.y)
+                self.y = (1 / math.tan(ar)) * self.y / abs(self.y)
             else:
                 self.y = 0
         else:
@@ -66,41 +74,44 @@ class Bol:
             self.x = 0
             self.y = 0
 
-    def get_par(self):
-        return self.x0 - self.r, self.y0 - self.r, self.x0 + self.r, self.y0 + self.r
+    def get_par(self) -> tuple[float, float, float, float]:
+        return (
+            self.x0 - self.r,
+            self.y0 - self.r,
+            self.x0 + self.r,
+            self.y0 + self.r,
+        )
 
-    def get_xy(self):
+    def get_xy(self) -> tuple[float, float]:
         return self.x0, self.y0
 
-    def get_radius(self):
+    def get_radius(self) -> float:
         return self.r
 
 
-def mouse_button_left():
-    x, y = pygame.mouse.get_pos()
+def mouse_button_left() -> None:
+    x, y = map(float, pygame.mouse.get_pos())
     x -= line[0]
     y -= line[1]
     x /= 100
     y /= 100
 
-    bot = Bol(line[0], line[1], 20, [x, y])
+    bot = Bol(line[0], line[1], 20, (x, y))
+    balls.append(bot)
 
-    bots.append(bot)
 
-
-def mouse_move():
+def mouse_move() -> None:
     line[2], line[3] = pygame.mouse.get_pos()
 
 
-def mouse_button_right():
+def mouse_button_right() -> None:
     line[0], line[1] = pygame.mouse.get_pos()
 
 
-def main_loop():
+def main_loop() -> None:
     clock = pygame.time.Clock()
 
     while True:
-
         for even in pygame.event.get():
             if even.type == pygame.MOUSEBUTTONUP:
                 if even.button == 1:
@@ -115,10 +126,10 @@ def main_loop():
         screen.fill('white')
         pygame.draw.line(screen, 'red', line[0: 2], line[2: 4])
 
-        for bot in bots:
+        for bot in balls:
             bot.run()
 
-        for bot in bots:
+        for bot in balls:
             pygame.draw.circle(screen, 'green', bot.get_xy(), bot.get_radius())
 
         pygame.display.flip()
@@ -131,8 +142,8 @@ if __name__ == '__main__':
 
     size = (500, 600)
     screen = pygame.display.set_mode(size)
-    img = Image.new('RGB', size)
-    img_dr = ImageDraw.ImageDraw(img)
-    bots = []
+    img = PIL.Image.new('RGB', size)
+    img_dr = PIL.ImageDraw.ImageDraw(img)
+    balls: list[Bol] = []
     line = [size[0] / 2, size[1] / 2, 0, 0]
     main_loop()
